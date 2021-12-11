47878bbc8e6651035612dafa2c5ed91292dbcbc2
Shuva_Biswas <shuvabiswas12@gmail.com>
2021-12-11 23:52:08 +0600

    first commit

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        #region :: hello world ::
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+        #endregion
+    }
+}
---
+++ InAndOut/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Models
+{
+    public class Item
+    {
+        public Item()
+        {
+
+        }
+
+        [Key]
+        public int id { get; set; }
+
+        public string borrower { get; set; }
+        
+        public string lender { get; set; }
+
+        public string itemName { get; set; }
+    }
+}
